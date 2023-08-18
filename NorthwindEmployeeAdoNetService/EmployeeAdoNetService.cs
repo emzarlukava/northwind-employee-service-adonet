ae7993d01ca3d02637dfb8e2c138d0d2384c6b3b
Anzhelika Kravchuk <AnzhelikaKravchuk@users.noreply.gitlab.com>
2023-08-18 14:47:01 +0300

    Add solution.

---
+++ NorthwindEmployeeAdoNetService/EmployeeAdoNetService.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace NorthwindEmployeeAdoNetService;
+
+/// <summary>
+/// A service for interacting with the "Employees" table using ADO.NET.
+/// </summary>
+public sealed class EmployeeAdoNetService
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="EmployeeAdoNetService"/> class.
+    /// </summary>
+    /// <param name="dbFactory">The database provider factory used to create database connection and command instances.</param>
+    /// <param name="connectionString">The connection string used to establish a database connection.</param>
+    /// <exception cref="ArgumentNullException">Thrown when either <paramref name="dbFactory"/> or <paramref name="connectionString"/> is null.</exception>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="connectionString"/> is empty or contains only white-space characters.</exception>
+    public EmployeeAdoNetService(DbProviderFactory dbFactory, string connectionString)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Retrieves a list of all employees from the Employees table of the database.
+    /// </summary>
+    /// <returns>A list of Employee objects representing the retrieved employees.</returns>
+    public IList<Employee> GetEmployees()
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Retrieves an employee with the specified employee ID.
+    /// </summary>
+    /// <param name="employeeId">The ID of the employee to retrieve.</param>
+    /// <returns>The retrieved an <see cref="Employee"/> instance.</returns>
+    /// <exception cref="EmployeeServiceException">Thrown if the employee is not found.</exception>
+    public Employee GetEmployee(long employeeId)
+    {
+        throw new NotImplementedException();
+    }
+    /// <summary>
+    /// Adds a new employee to Employee table of the database.
+    /// </summary>
+    /// <param name="employee">The  <see cref="Employee"/> object containing the employee's information.</param>
+    /// <returns>The ID of the newly added employee.</returns>
+    /// <exception cref="EmployeeServiceException">Thrown when an error occurs while adding the employee.</exception>
+    public long AddEmployee(Employee employee)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Removes an employee from the the Employee table of the database based on the provided employee ID.
+    /// </summary>
+    /// <param name="employeeId">The ID of the employee to remove.</param>
+    /// <exception cref="EmployeeServiceException"> Thrown when an error occurs while attempting to remove the employee.</exception>
+    public void RemoveEmployee(long employeeId)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Updates an employee record in the Employee table of the database.
+    /// </summary>
+    /// <param name="employee">The employee object containing updated information.</param>
+    /// <exception cref="EmployeeServiceException">Thrown when there is an issue updating the employee record.</exception>
+    public void UpdateEmployee(Employee employee)
+    {
+        throw new NotImplementedException();
+    }
+}
